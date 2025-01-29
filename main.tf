a481541c445f0e12636918fe6cd18e5a17c09792
Aleksey Kim <alex.romanovich.ak@gmail.com>
2025-01-29 16:34:49 +0900

    feat: test on azure

--- main.tf
+++ main.tf
@@ -1,12 +1,49 @@
 terraform {
-	required_providers{
-		azurerm = {
-			source = "hashicorp/azurerm"
-			version = "~> 3.0"
-		}
-	}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
-provider "azurerm"{
-	features{}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+
+}
+
+
+
+resource "azurerm_service_plan" "example" {
+  name                = "example-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "example-web-app-code-seoul" # Must be globally unique
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {
+    container_registry_use_managed_identity = false
+
+    application_stack {
+      docker_image_name   = "nginx:latest"
+      docker_registry_url = "https://index.docker.io"
+    }
+  }
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+  }
 }
